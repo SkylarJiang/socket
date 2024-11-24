4fcd3327d22cad4fa93624bbecedbde862270511
Layne <7821035+laynehsu@user.noreply.gitee.com>
2024-11-24 22:19:45 +0800

    [socket] Improve code by wrapper class to

--- MessageBoardClient.py
+++ MessageBoardClient.py
@@ -1,25 +1,23 @@
 import socket
 import sys
 
-# Task 1: Socket initialization
-def create_socket():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.settimeout(10)
-    return client_socket
+class SocketWrapper:
+    # Task 1: Socket initialization
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(10)
 
-# Task 2: Initiate the connection request
-def connect_to_server(client_socket, server_ip, server_port):
-    server_address = (server_ip, server_port)
-    client_socket.connect(server_address)
+    # Task 2: Initiate the connection request
+    def connect(self, host, port):
+        self.socket.connect((host, port))
 
-# Task 3: Send command to the server
-def send_command(client_socket, msg):
-    client_socket.sendall(msg.encode('utf-8'))
+    # Task 3: Send command to the server
+    def send(self, msg):
+        self.socket.send(msg.encode('utf-8'))
 
-# Task 4: Receive the message from the server
-def handle_response(client_socket):
-    try:
-        response = client_socket.recv(1024)
+    # Task 4: Receive the message from the server
+    def receive(self):
+        response = self.socket.recv(1024)
         if response:
             response_str = response.decode('utf-8').strip()
             print(f"server: {response_str}")
@@ -27,21 +25,9 @@ def handle_response(client_socket):
         else:
             print("No data received, connection might be closed.")
             return ""
-    except socket.timeout:
-        print("Receive timed out, no data received within the timeout period.")
-        return ""
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return ""
 
-# Handle the data input by users in POST and DELETE
-def handle_input(client_socket):
-    while True:
-        user_input = input("client: ")
-        send_command(client_socket, user_input)
-        if (user_input == '#'): 
-            handle_response(client_socket)
-            break
+    def close(self):
+        self.socket.close()
 
 # Main function to run the client
 def main():
@@ -52,31 +38,40 @@ def main():
         server_ip = sys.argv[1]
         server_port = int(sys.argv[2])
 
-    client_socket = create_socket()
+    client_socket = SocketWrapper()
 
     try:
-        connect_to_server(client_socket, server_ip, server_port)
+        client_socket.connect(server_ip, server_port)
 
         while True:
             command = input("client: ").upper()
-            send_command(client_socket, command)
+            client_socket.send(command)
             if command == 'POST' or command == 'DELETE':
-                handle_input(client_socket)
-            elif command == 'GET':
+                # Handle the data input by users in POST and DELETE
                 while True:
-                    response = handle_response(client_socket)
+                    user_input = input("client: ")
+                    client_socket.send(user_input)
+                    if (user_input == '#'): 
+                        client_socket.receive()
+                        break
+            elif command == 'GET':
+                # Print the data received from server
+                while True:
+                    response = client_socket.receive()
                     if response == "#":
                         break
             elif command == 'QUIT':
-                response = handle_response(client_socket)
+                response = client_socket.receive()
                 if response == "OK":
                     # Task 5: Close socket
                     client_socket.close()
                     break
             else:
-                handle_response(client_socket)
+                client_socket.receive()
     except KeyboardInterrupt:
         print("\nProgram terminated by user.")
+    except socket.timeout:
+        print("Receive timed out, no data received within the timeout period.")
     except Exception as e:
         print(f"An error occurred: {e}")
         client_socket.close()
